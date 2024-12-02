634bda16e95bd447fd834f8085497a3e0f79d2db
niccolo.bergamini <niccolo.bergamini@qooa.it>
2024-12-02 13:15:34 +0100

    day 02 - part1

---
+++ src/Day02.kt
@@ -0,0 +1,25 @@
+import kotlin.math.abs
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.count {
+            it -> it.isSafe()
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
+
+fun String.isSafe(): Boolean {
+    var elements = this.split(" ").map{it.toInt()}
+    return elements.filterIndexed { i, it ->
+        i < elements.size - 1 && (!(abs(elements[i]-elements[i+1]) in 1..3) || ((elements[0] > elements[1]) && elements[i] < elements[i + 1] || !(elements[0] > elements[1]) && elements[i] > elements[i + 1]))
+    }.isEmpty()
+}
