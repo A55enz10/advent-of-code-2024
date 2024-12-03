0a02688f2fce7c711a214882af35979589bacdc6
niccolo.bergamini <niccolo.bergamini@qooa.it>
2024-12-03 16:07:45 +0100

    day 03 - part2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,27 +1,16 @@
 fun main() {
 
     fun part1(input: List<String>): Int {
-
-        val solution1 = input.sumOf{
+        return input.sumOf{
             Regex("mul\\(\\d+,\\d+\\)").findAll(it)
             .map { it.value.substring(4, it.value.length-1).split(",") }
             .toList()
             .sumOf { it[0].toInt() * it[1].toInt() }
         }
-
-        val solution2 = input.sumOf{
-            it.split("mul(")
-            .map {if (it.indexOf(")") > 1) it.substring(0, it.indexOf(")")) else null }
-            .filter { s -> s != null && s.matches(Regex("""^-?\d+,-?\d+${'$'}""")) }
-            .map { s2 -> s2?.split(",") }
-            .sumOf { values -> values!![0].toInt() * values[1].toInt() }
-        }
-
-        return solution1
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return part1(input.joinToString().split("do()").map { if (it.contains("don't()"))  it.substring(0, it.indexOf("don't()")) else it })
     }
 
     val input = readInput("Day03")
